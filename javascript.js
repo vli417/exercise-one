a3d2bbfbfb29ed0ff7c9cb86338238c1678f39af
Ivy Li <ivyli@10-18-194-171.dynapool.wireless.nyu.edu>
2023-09-12 14:38:51 -0400

    new commit

---
+++ javascript.js
@@ -0,0 +1,40 @@
+
+# JavaScript
+
+
+## Functions
+
+function coolFunction(arg){
+    if(typeof arg === 'string;') return 'string';
+    
+    return 'not string';
+
+
+
+}
+const thisval= coolFunction('hi')
+const coolArrowfunction = () => 'hello';
+   
+
+
+## Objects
+
+const annoyingObject = {
+    top [
+        {
+            middle: {
+                bottom: {
+                    things: 'frogs'
+
+                }
+            }
+        }
+    ]
+}
+
+annoyingObject.top[0].middle.bottom.thing // 'frogs'
+const JSONExample = {
+    {
+        ""
+    }
+}
